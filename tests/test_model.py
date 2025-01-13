1f68306f8ac655861a63c502466ceb364df5deb3
moskit <tomek_niedzi@o2.pl>
2025-01-13 12:46:36 +0100

    failing test to check action time

--- tests/test_model.py
+++ tests/test_model.py
@@ -4,3 +4,9 @@ from exe_project.model import MyAwesomeModel
 import torch
 
 
+def test_error_on_wrong_shape():
+    model = MyAwesomeModel()
+    # with pytest.raises(ValueError, match="Expected input to a 4D tensor"):
+    #     model(torch.randn(1, 2, 3))
+    with pytest.raises(ValueError, match="Expected each sample to have shape [1, 28, 28]"):
+        model(torch.randn(1, 1, 28, 29))
