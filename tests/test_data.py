7c78bea0cf842694b106d67114021f0910ff0b4e
moskit <tomek_niedzi@o2.pl>
2025-01-13 15:54:43 +0100

    added dvc to workflow and enabled data test

--- tests/test_data.py
+++ tests/test_data.py
@@ -5,7 +5,7 @@ from exe_project.data import corrupt_mnist
 
 # PULL FROM DVC NEEDED TO ACCESS DATA IN THE TEST -> THAT REQUIRES AUTHENTICATION FOR GITHUB
 
-# def test_my_dataset():
-#     """Test the MyDataset class."""
-#     train_set, _ = corrupt_mnist()
-#     assert isinstance(train_set, Dataset)
+def test_my_dataset():
+    """Test the MyDataset class."""
+    train_set, _ = corrupt_mnist()
+    assert isinstance(train_set, Dataset)
