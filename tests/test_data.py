915098d36246473debe993bf9ffc5e030da83863
moskit <tomek_niedzi@o2.pl>
2025-01-13 12:31:56 +0100

    accessing data path from the project path

--- tests/test_data.py
+++ tests/test_data.py
@@ -2,8 +2,10 @@ from torch.utils.data import Dataset
 
 from exe_project.data import corrupt_mnist
 
+from tests import _PATH_DATA
+
 
 def test_my_dataset():
     """Test the MyDataset class."""
-    train_set, _ = corrupt_mnist()
+    train_set, _ = corrupt_mnist(_PATH_DATA)
     assert isinstance(train_set, Dataset)
