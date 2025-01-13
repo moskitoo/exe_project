9fb4535a7bd3190738cc5847a404da330ccd74da
moskit <tomek_niedzi@o2.pl>
2025-01-13 16:06:27 +0100

    ruff cleanup to be tested in action

--- tests/test_training.py
+++ tests/test_training.py
@@ -8,6 +8,7 @@ from sklearn.metrics import RocCurveDisplay, accuracy_score, f1_score, precision
 
 from exe_project.data import corrupt_mnist
 from exe_project.model import MyAwesomeModel
+
 # from exe_project.src.exe_project import train
 from tests import _PROJECT_ROOT
 
@@ -18,16 +19,16 @@ def test_training():
 
     # training_cfg = OmegaConf.load(Path(_PROJECT_ROOT) / "configs" / "model_conf.yaml")
     loss_fn = torch.nn.CrossEntropyLoss()
-    
+
     # Mock data
     batch_size = 4
     num_classes = 10
     logits = torch.randn(batch_size, num_classes, requires_grad=True)  # Random predictions (logits)
     targets = torch.randint(0, num_classes, (batch_size,))  # Random valid class indices
-    
+
     # Compute loss
     loss = loss_fn(logits, targets)
-    
+
     # Assertions
     assert isinstance(loss.item(), float), "Loss is not a scalar."
     assert loss.item() >= 0, "Loss is negative."
